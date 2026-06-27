d48facae960aa650ea919f23e0d7da1b039092fa
Tezzar <alimimsoul@gmail.com>
2026-06-27 12:42:45 +0330

    add loader

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <div className='spinner'>
+      <div className='loader'></div>
+      <p className='loader-text'>PLEASE WAIT . . .</p>
+    </div>
+    </div>
+  );
+}
+
+export default App;
